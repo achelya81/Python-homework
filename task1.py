f582cece47c07ef86e0bb20970940522a7ce6976
achelya81 <achelya81@gmail.com>
2025-06-29 21:25:43 +0000

    homework task1 years

---
+++ task1.py
@@ -0,0 +1,12 @@
+year=int(input("Enter numbers of the years: "))
+
+days=year*365
+weeks=days//7
+hours=days*24
+
+print(f"in {year} years {year*365} days")
+print(f"in {days} days")
+print(f"in {weeks} weeks")
+print(f"in {hours} hours")
+
+
