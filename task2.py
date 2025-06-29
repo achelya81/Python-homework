0cf79bc0766b6bad87a51f9680c8e4e8d73ecc64
achelya81 <achelya81@gmail.com>
2025-06-29 22:43:25 +0000

    food list

---
+++ task2.py
@@ -0,0 +1,16 @@
+foods=("apple-(52 kcal)", 
+"banana", 
+"orange", 
+"mango", 
+"strawberry", 
+"mandarin",
+"strawberry")
+calories=("(52 kcal)", 
+"(96 kcal)", 
+"(62 kcal)", 
+"(605 kcal)", 
+"(33-kcal)", 
+"(50 kcal)",
+"(33 kcal)")
+print(foods)
+print(calories)
---
+++ unique.py
@@ -0,0 +1,19 @@
+foods=["apple-(52 kcal)", 
+"banana", 
+"orange", 
+"mango", 
+"strawberry", 
+"mandarin",
+"strawberry"]
+calories=["(52 [kcal)", 
+"(96 kcal)", 
+"(62 kcal)", 
+"(605 kcal)", 
+"(33-kcal)", 
+"(50 kcal)",
+"(33 kcal)"]
+print(foods[6])
+print(calories[-3])
+
+print(foods[-2])
+print(calories[-2])
